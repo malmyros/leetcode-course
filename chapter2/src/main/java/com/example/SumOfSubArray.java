33c9dec771ae582411e683abf667c895a846e1fb
michail.almyros <m.almyros@gmail.com>
2025-05-19 20:07:17 +0100

    - Adding additional examples

---
+++ chapter2/src/main/java/com/example/SumOfSubArray.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class SumOfSubArray {
+
+    public static void main(String[] args) {
+
+        int start = 2;
+        int end = 6;
+        int[] nums = new int[]{3, 6, 2, 8, 1, 4, 1, 5};
+        System.out.println(sumOfSubArray(nums, start, end));
+    }
+
+    private static int sumOfSubArray(int[] nums, int start, int end) {
+
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        return prefix[end] - prefix[start];
+    }
+}
