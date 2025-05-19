c35e3766e5ab3ae91fda1b4fc9ea5187942e2bbc
michail.almyros <m.almyros@gmail.com>
2025-05-19 13:55:06 +0100

    first commit

---
+++ chapter2/src/main/java/com/example/IsPalindrome.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class IsPalindrome {
+
+
+    /*
+        Given an input string determine
+        if the input is a palindrome.
+     */
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("madam"));
+        System.out.println(isPalindrome("hello"));
+    }
+
+    private static boolean isPalindrome(String input) {
+
+        int n = input.length();
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+
+            int a = input.charAt(left);
+            int b = input.charAt(right);
+
+            if (a != b) {
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+}
