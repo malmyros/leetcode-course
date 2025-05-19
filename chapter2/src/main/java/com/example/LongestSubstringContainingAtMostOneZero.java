d278aa2921eec904bff946dc11d4f3bfdaa24202
michail.almyros <m.almyros@gmail.com>
2025-05-19 16:20:15 +0100

    - Added an exercise for finding the longest substring with at least one 0

---
+++ chapter2/src/main/java/com/example/LongestSubstringContainingAtMostOneZero.java
@@ -0,0 +1,39 @@
+package com.example;
+
+public class LongestSubstringContainingAtMostOneZero {
+
+    /**
+     * Find the longest substring containing at most
+     * 1 zero in a given string
+     */
+    public static void main(String[] args) {
+
+        String input = "1101100111";
+        System.out.println(longestSubString(input));
+    }
+
+    private static int longestSubString(String s) {
+
+        int left = 0;
+        int curr = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            if (s.charAt(right)== '0') {
+                curr++;
+            }
+
+            while (curr > 1) {
+                if (s.charAt(left) == '0') {
+                    curr--;
+                }
+
+                left++;
+            }
+
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
