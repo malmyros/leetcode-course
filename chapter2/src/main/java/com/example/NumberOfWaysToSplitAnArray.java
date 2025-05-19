95907383bedaa2accf7b8365671dd7c3320a5190
michail.almyros <m.almyros@gmail.com>
2025-05-19 20:36:02 +0100

    - Adding additional examples

---
+++ chapter2/src/main/java/com/example/NumberOfWaysToSplitAnArray.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public class NumberOfWaysToSplitAnArray {
+
+    /**
+     * Given an integer array nums, find the number of ways to split the
+     * array into two parts so that the first section has a sum greater
+     * than or equal to the sum of the second section.
+     * <p/>
+     * The second section should have at least one number.
+     */
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{10, 4, -8, 7};
+        System.out.println(numberOfWaysToSplit(nums));
+    }
+
+    private static int numberOfWaysToSplit(int[] nums) {
+
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = prefix[i -1] + nums[i];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < nums.length -1; i++) {
+            int right = prefix[i];
+            int left = prefix[nums.length -1] - prefix[i];
+            if (right >= left) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
