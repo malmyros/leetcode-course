b6b11c79d81a97a01eb9c6a8577c491bccf54b8a
michail.almyros <m.almyros@gmail.com>
2025-05-20 17:00:14 +0100

    - Added an example for summing up all the values on the ListNode recursively

---
+++ linkedlists/src/main/java/com/example/main/SumOfNodesRecursively.java
@@ -0,0 +1,25 @@
+package com.example.main;
+
+public class SumOfNodesRecursively {
+
+    public static void main(String[] args) {
+
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        ListNode three = new ListNode(3);
+
+        one.next = two;
+        two.next = three;
+
+        System.out.println(sumOfNodes(one));
+    }
+
+    private static int sumOfNodes(ListNode listNode) {
+
+        if (listNode == null) {
+            return 0;
+        }
+
+        return listNode.val + sumOfNodes(listNode.next);
+    }
+}
