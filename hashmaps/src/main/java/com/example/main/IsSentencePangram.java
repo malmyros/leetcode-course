ac6483b7b14e8b771ab302136cf7dba146588690
michail.almyros <m.almyros@gmail.com>
2025-05-20 11:10:31 +0100

    - Added additional exercises

--- hashmaps/src/main/java/com/example/main/IsSentencePangram.java
+++ hashmaps/src/main/java/com/example/main/IsSentencePangram.java
@@ -1,8 +1,6 @@
 package com.example.main;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class IsSentencePangram {
@@ -18,9 +16,7 @@ public class IsSentencePangram {
         Set<Character> characterSet = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (!characterSet.contains(c)) {
-                characterSet.add(c);
-            }
+            characterSet.add(c);
         }
 
         return characterSet.size() == 26;
