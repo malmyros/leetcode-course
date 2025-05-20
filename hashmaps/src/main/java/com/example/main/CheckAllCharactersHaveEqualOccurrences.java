393f9d3f7089a53a1a5f1f365e05df86a3b6fced
michail.almyros <m.almyros@gmail.com>
2025-05-20 12:42:29 +0100

    - Adding additional exercises

---
+++ hashmaps/src/main/java/com/example/main/CheckAllCharactersHaveEqualOccurrences.java
@@ -0,0 +1,34 @@
+package com.example.main;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CheckAllCharactersHaveEqualOccurrences {
+
+    /**
+     * Given a string s, determine if all characters have the same frequency.
+     */
+    public static void main(String[] args) {
+
+        String s = "abacbc";
+        System.out.println(sameFrequency(s));
+    }
+
+    private static boolean sameFrequency(String s) {
+
+        Map<Character, Integer> frequency = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (Character c : frequency.keySet()) {
+            set.add(frequency.get(c));
+        }
+
+        return set.size() == 1;
+    }
+}
