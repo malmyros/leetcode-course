21a6a18ea59ec373ae5bbecb621721795be2c849
michail.almyros <m.almyros@gmail.com>
2025-05-20 11:39:27 +0100

    - Added additional exercises

---
+++ hashmaps/src/main/java/com/example/main/MissingNumber.java
@@ -0,0 +1,34 @@
+package com.example.main;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingNumber {
+
+    /**
+     * Given an array nums containing n distinct numbers in the
+     * range [0, n], return the only number in the range that is missing from the array.
+     */
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{3, 0, 1};
+//        int[] nums = new int[]{0, 1};
+        System.out.println(missingNumber(nums));
+    }
+
+    private static int missingNumber(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
