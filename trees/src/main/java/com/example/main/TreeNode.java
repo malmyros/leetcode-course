4539c314bb0584c476b980ac0f4d6200481d871d
michail.almyros <m.almyros@gmail.com>
2025-05-24 08:49:36 +0100

    - Adding a new module for trees

---
+++ trees/src/main/java/com/example/main/TreeNode.java
@@ -0,0 +1,14 @@
+package com.example.main;
+
+public class TreeNode {
+
+    private int val;
+
+    private TreeNode left;
+
+    private TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
