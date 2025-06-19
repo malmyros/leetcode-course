dab1497cfff808fde1b8191085c0254a89271422
michail.almyros <m.almyros@gmail.com>
2025-06-19 13:02:59 +0100

    - Added an example for finding minimum depth of a binary tree, and the max ancestor difference between nodes

---
+++ trees/src/main/java/com/example/main/MaxAncestorDifference.java
@@ -0,0 +1,58 @@
+package com.example.main;
+
+public class MaxAncestorDifference {
+
+    static int result = 0;
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(8);
+        TreeNode three = new TreeNode(3);
+        TreeNode one = new TreeNode(1);
+        TreeNode six = new TreeNode(6);
+        TreeNode four = new TreeNode(4);
+        TreeNode seven = new TreeNode(7);
+        TreeNode ten = new TreeNode(10);
+        TreeNode fourteen = new TreeNode(14);
+        TreeNode thirteen = new TreeNode(13);
+
+        root.left = three;
+        three.left = one;
+        three.right = six;
+        six.left = four;
+        six.right = seven;
+
+        root.right = ten;
+        ten.right = fourteen;
+        fourteen.right = thirteen;
+
+        int result = maxAncestorDiff(root);
+        System.out.println(result);
+    }
+
+    private static int maxAncestorDiff(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        result = 0;
+        helper(root, root.val, root.val);
+        return result;
+    }
+
+    private static void helper(TreeNode node, int curMax, int curMin) {
+
+        if (node == null) {
+            return;
+        }
+
+        int possibleResult = Math.max(Math.abs(curMax - node.val), Math.abs(curMin - node.val));
+        result = Math.max(result, possibleResult);
+
+        curMax = Math.max(curMax, node.val);
+        curMin = Math.min(curMin, node.val);
+        helper(node.left, curMax, curMin);
+        helper(node.right, curMax, curMin);
+    }
+}
---
+++ trees/src/main/java/com/example/main/MinDepth.java
@@ -0,0 +1,40 @@
+package com.example.main;
+
+public class MinDepth {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        TreeNode nine = new TreeNode(9);
+        TreeNode twenty = new TreeNode(20);
+        TreeNode fifteen = new TreeNode(15);
+        TreeNode seven = new TreeNode(7);
+
+        root.left = nine;
+        root.right = twenty;
+        twenty.left = fifteen;
+        twenty.right = seven;
+
+        int ans = minDepth(root);
+        System.out.println(ans);
+    }
+
+    private static int minDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return Math.min(left, right) + 1;
+    }
+}
